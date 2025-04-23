52a23df60197df1ec8220c0f61ff10ebee961f4d
irfanathoombayil <irfanashahjahan73@gmail.com>
2025-04-23 14:55:37 +0530

    Update newscontroller.js

--- backend/controller/newscontroller.js
+++ backend/controller/newscontroller.js
@@ -18,7 +18,7 @@ console.log("newscontroller")
        let newNews = News(req.body)
        newNews.save().then((newnews)=>{
            if(newnews){
-               return res.status(200).json({message:"News created.."})
+               return res.status(200).json({message:"News created...."})
            }
            else{
                return res.status(400).json({message:"Unable to create news..................."})
